b1e6f0d36bd169280565754d120eae010275659f
MartinaRistic <ristic214@gmail.com>
2020-04-09 23:27:48 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -8,6 +8,22 @@ class Point {
 	//TODO constructor
 
 	//TODO setters and getters
+	Point(){}
+    Point(double a, double b)
+    {
+        x=a;
+        y=b;
+
+    }
+    ~Point(){}
+    void set_x(double a)
+    {
+        x=a;
+    }
+    void set_y(double b)
+    {
+        y=b;
+    }
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
